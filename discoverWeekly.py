c9dc6447e921d0982b6e4b9331031c058eac9211
Katia Gilligan <synsation88@gmail.com>
2023-03-19 18:16:45 -0700

    Added discover weekly playlist

---
+++ discoverWeekly.py
@@ -0,0 +1,109 @@
+# import necessary modules
+import time
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, request, url_for, session, redirect
+
+# initialize Flask app
+app = Flask(__name__)
+
+# set the name of the session cookie
+app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie'
+
+# set a random secret key to sign the cookie
+app.secret_key = 'YOUR_SECRET_KEY'
+
+# set the key for the token info in the session dictionary
+TOKEN_INFO = 'token_info'
+
+# route to handle logging in
+@app.route('/')
+def login():
+    # create a SpotifyOAuth instance and get the authorization URL
+    auth_url = create_spotify_oauth().get_authorize_url()
+    # redirect the user to the authorization URL
+    return redirect(auth_url)
+
+# route to handle the redirect URI after authorization
+@app.route('/redirect')
+def redirect_page():
+    # clear the session
+    session.clear()
+    # get the authorization code from the request parameters
+    code = request.args.get('code')
+    # exchange the authorization code for an access token and refresh token
+    token_info = create_spotify_oauth().get_access_token(code)
+    # save the token info in the session
+    session[TOKEN_INFO] = token_info
+    # redirect the user to the save_discover_weekly route
+    return redirect(url_for('save_discover_weekly',_external=True))
+
+# route to save the Discover Weekly songs to a playlist
+@app.route('/saveDiscoverWeekly')
+def save_discover_weekly():
+    try: 
+        # get the token info from the session
+        token_info = get_token()
+    except:
+        # if the token info is not found, redirect the user to the login route
+        print('User not logged in')
+        return redirect("/")
+
+    # create a Spotipy instance with the access token
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+
+    # get the user's playlists
+    current_playlists =  sp.current_user_playlists()['items']
+    discover_weekly_playlist_id = None
+    saved_weekly_playlist_id = None
+
+    # find the Discover Weekly and Saved Weekly playlists
+    for playlist in current_playlists:
+        if(playlist['name'] == 'Discover Weekly'):
+            discover_weekly_playlist_id = playlist['id']
+        if(playlist['name'] == 'Saved Weekly'):
+            saved_weekly_playlist_id = playlist['id']
+    
+    # if the Discover Weekly playlist is not found, return an error message
+    if not discover_weekly_playlist_id:
+        return 'Discover Weekly not found.'
+    
+    # get the tracks from the Discover Weekly playlist
+    discover_weekly_playlist = sp.playlist_items(discover_weekly_playlist_id)
+    song_uris = []
+    for song in discover_weekly_playlist['items']:
+        song_uri= song['track']['uri']
+        song_uris.append(song_uri)
+    
+    # add the tracks to the Saved Weekly playlist
+    sp.user_playlist_add_tracks("YOUR_USER_ID", saved_weekly_playlist_id, song_uris, None)
+
+    # return a success message
+    return ('Discover Weekly songs added successfully')
+
+# function to get the token info from the session
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        # if the token info is not found, redirect the user to the login route
+        redirect(url_for('login', _external=False))
+    
+    # check if the token is expired and refresh it if necessary
+    now = int(time.time())
+
+    is_expired = token_info['expires_at'] - now < 60
+    if(is_expired):
+        spotify_oauth = create_spotify_oauth()
+        token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
+
+    return token_info
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id = 'YOUR_CLIENT_ID',
+        client_secret = 'YOUR_CLIENT_SECRET',
+        redirect_uri = url_for('redirect_page', _external=True),
+        scope='user-library-read playlist-modify-public playlist-modify-private'
+    )
+
+app.run(debug=True)
